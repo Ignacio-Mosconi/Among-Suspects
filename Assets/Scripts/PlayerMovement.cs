11ffa44c32aa38a41aa60795eff7c72d2dc1cd14
Ignacio Mosconi <nacho7577@hotmail.com>
2019-04-11 16:08:48 -0300

    Added First Person Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] [Range(5f, 10f)] float movementSpeed = 5f;
+
+    CharacterController characterController;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 inputVector;
+        Vector3 movement = Vector3.zero;
+        float horMovement = Input.GetAxis("Horizontal");
+        float forMovement = Input.GetAxis("Vertical");
+
+        inputVector = new Vector3(horMovement, 0f, forMovement).normalized;
+        movement += (transform.right * inputVector.x + transform.forward * inputVector.z) * movementSpeed;
+
+        characterController.Move(movement * Time.deltaTime);
+    }
+}
