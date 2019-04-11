ab3b79806b7187a3246da61541975d3466087a51
Ignacio Mosconi <nacho7577@hotmail.com>
2019-04-11 15:48:44 -0300

    Added First Person Camera Rotation

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    [SerializeField] [Range(90f, 135f)] float rotationSpeed = 100f;
+    [SerializeField] [Range(50f, 90f)] float verticalRange = 80f;
+    
+    Transform fpsCamera;
+    float horAngle = 0f;
+    float verAngle = 0f;
+
+    void Awake()
+    {
+        fpsCamera = GetComponentInChildren<Camera>().transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float horRotation = Input.GetAxis("Mouse X");
+        float verRotation = Input.GetAxis("Mouse Y");
+
+        horAngle += horRotation * rotationSpeed * Time.deltaTime;
+        verAngle -= verRotation * rotationSpeed * Time.deltaTime;
+
+        verAngle = Mathf.Clamp(verAngle, -verticalRange, verticalRange);
+
+        transform.eulerAngles = new Vector3(transform.rotation.x, horAngle, transform.rotation.z);
+        fpsCamera.localEulerAngles = new Vector3(verAngle, fpsCamera.rotation.y, fpsCamera.rotation.z);
+    }
+}
