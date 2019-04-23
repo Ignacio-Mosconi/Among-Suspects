c03279d160a3d59f4f0b0c76294a695394715c7f
Gaspar Nuñez <gaspacho09@hotmail.com>
2019-04-23 12:23:31 -0300

    Run multiplier And Movement Buff
    Movement increased (2.5 -> 3) , multiplied when pressing shift (2x by default).

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] [Range(2f, 5f)] float movementSpeed = 3f;
+	[SerializeField] [Range(2f, 3f)] float runMultiplier = 2f;
+    private float mult = 1.0f;
 
     CharacterController characterController;
 
@@ -20,8 +22,11 @@ public class PlayerMovement : MonoBehaviour
         float forMovement = Input.GetAxis("Vertical");
 
         inputVector = new Vector3(horMovement, 0f, forMovement).normalized;
-        movement += (transform.right * inputVector.x + transform.forward * inputVector.z) * movementSpeed;
-
+        if (Input.GetAxisRaw("Run") == 1){
+            mult = runMultiplier;
+        }
+        else mult = 1.0f;
+        movement += (transform.right * inputVector.x + transform.forward * inputVector.z) * movementSpeed * mult;
         characterController.Move(movement * Time.deltaTime);
     }
 }
