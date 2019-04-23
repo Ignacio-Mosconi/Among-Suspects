c23cb5d437d45bfb0a20bf5016ca819ed131490d
Ignacio Mosconi <nacho7577@hotmail.com>
2019-04-23 15:01:52 -0300

    Refactored the "Player Movement" Script
    - Renamed some variables and refactored some code.
    - Made some changes to the Input Manager asset.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] [Range(2f, 5f)] float movementSpeed = 3f;
 	[SerializeField] [Range(2f, 3f)] float runMultiplier = 2f;
-    private float mult = 1.0f;
 
     CharacterController characterController;
+    float currentRunMultiplier = 1f;
 
     void Awake()
     {
@@ -18,21 +18,13 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 inputVector;
         Vector3 movement = Vector3.zero;
-        float horMovement = 0.0f;
-        float forMovement = 0.0f;
-        if (Input.GetButton("Horizontal")){
-             horMovement = Input.GetAxis("Horizontal");
-        }
-        if (Input.GetButton("Vertical")){
-            forMovement = Input.GetAxis("Vertical");
-        }
+
+        float horMovement = Input.GetAxis("Horizontal");
+        float forMovement = Input.GetAxis("Vertical");
 
         inputVector = new Vector3(horMovement, 0f, forMovement).normalized;
-        if (Input.GetAxisRaw("Run") == 1){
-            mult = runMultiplier;
-        }
-        else mult = 1.0f;
-        movement += (transform.right * inputVector.x + transform.forward * inputVector.z) * movementSpeed * mult;
+        currentRunMultiplier = (Input.GetButton("Run")) ? runMultiplier : 1f;
+        movement += (transform.right * inputVector.x + transform.forward * inputVector.z) * movementSpeed * currentRunMultiplier;
         characterController.Move(movement * Time.deltaTime);
     }
 }
