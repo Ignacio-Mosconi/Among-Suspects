4f74db59fb8751d97d67495a6c1cfc6bc90823c2
Ignacio Mosconi <nacho7577@hotmail.com>
2019-06-23 15:37:37 -0300

    Added an 'Investigation' Tutorial
    - Included a tutorial that explains the basics about the 'investigation'.

--- Assets/Scripts/Others/TutorialController.cs
+++ Assets/Scripts/Others/TutorialController.cs
@@ -3,32 +3,49 @@ using UnityEngine;
 
 public class TutorialController : MonoBehaviour
 {
-    [SerializeField] [Range(0.5f, 1f)] float tutorialDisplayDelay = 0.75f;
+    [SerializeField] [Range(0.5f, 2.5f)] float navigationTutorialDelay = 0.75f;
+    [SerializeField] [Range(0.5f, 2.5f)] float investigationTutorialDelay = 0.75f;
     
     TutorialInfo[] tutorials;
 
     void Start()
     {
         tutorials = Resources.LoadAll<TutorialInfo>("Tutorials");
+        
         DialogueManager.Instance.OnDialogueAreaDisable.AddListener(TriggerNavigationTutorial);
+        CharacterManager.Instance.PlayerController.OnStartedInvestigation.AddListener(TriggerInvestigationTutorial);
+    }
+
+    TutorialInfo FetchTutorial(TutorialType tutorialType)
+    {
+        TutorialInfo tutorial = Array.Find(tutorials, t => t.tutorialType == tutorialType);
+        if (!tutorial)
+            Debug.LogError("There are no '" + tutorialType.ToString() + "' tutorials set up");
+
+        return tutorial;
     }
 
     void TriggerNavigationTutorial()
     {
-        Invoke("DisplayNavigationTutorial", tutorialDisplayDelay);
+        Invoke("DisplayNavigationTutorial", navigationTutorialDelay);
+    }
+
+    void TriggerInvestigationTutorial()
+    {
+        Invoke("DisplayInvestigationTutorial", investigationTutorialDelay);
     }
 
     void DisplayNavigationTutorial()
     {
-        TutorialInfo tutorial = Array.Find(tutorials, t => t.tutorialType == TutorialType.Navigation);
-
-        if (!tutorial)
-        {
-            Debug.LogError("There are no 'Navigation' tutorials set up");
-            return;
-        }
-
+        TutorialInfo tutorial = FetchTutorial(TutorialType.Navigation);
         DialogueManager.Instance.OnDialogueAreaDisable.RemoveListener(TriggerNavigationTutorial);
+        DialogueManager.Instance.StartDialogue(tutorial);
+    }
+
+    void DisplayInvestigationTutorial()
+    {
+        TutorialInfo tutorial = FetchTutorial(TutorialType.Investigation);
+        CharacterManager.Instance.PlayerController.OnStartedInvestigation.RemoveListener(TriggerInvestigationTutorial);
         DialogueManager.Instance.StartDialogue(tutorial);
     }
 }
